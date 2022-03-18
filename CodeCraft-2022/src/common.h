a16dee04ef9a858743b70c641e17a3ebe3ee4290
cwher <cwher@outlook.com>
2022-03-18 14:03:57 +0800

    fix: upd framework

--- CodeCraft-2022/src/common.h
+++ CodeCraft-2022/src/common.h
@@ -15,14 +15,20 @@
 #include <tuple>
 #include <utility>
 #include <exception>
+#include <ctime>
+#include <chrono>
 
 typedef int i32;
 typedef unsigned int u32;
 typedef long long i64;
 typedef unsigned long long u64;
+typedef float f32;
+typedef double f64;
 
+using std::make_pair;
 using std::make_shared;
 using std::make_tuple;
+using std::pair;
 using std::queue;
 using std::shared_ptr;
 using std::string;
---
+++ CodeCraft-2022/src/settings.h
@@ -0,0 +1,17 @@
+#ifndef __SETTINGS_H__
+#define __SETTINGS_H__
+
+#include "common.h"
+
+namespace Settings
+{
+    // >>> minimize maximum local search
+    const i32 min_max_delta = 100000;
+
+    const f32 discounting_factor = 0.98;
+
+    // <<< minimize maximum local search
+
+};
+
+#endif
